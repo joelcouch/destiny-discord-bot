7adf523693b4eace25945b78177439abe8959d48
joel-hirealoo <joel@hirealoo.com.au>
2018-07-30 23:06:46 +1000

    Fix bot

--- bot.py
+++ bot.py
@@ -20,7 +20,20 @@ print('Launching bot...', end="", flush=True)
 client = commands.Bot(description=description, command_prefix=prefix)
 scheduler = sched.scheduler(time.time, time.sleep)
 
-discord.opus.load_opus()
+opus_libs = ['libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
+
+
+def load_opus_lib():
+    if discord.opus.is_loaded():
+        return True
+
+    for opus_lib in opus_libs:
+        try:
+            discord.opus.load_opus(opus_lib)
+            return
+        except OSError:
+            pass
+
 
 lfg_channels = ['lfg', 'raid', 'looking-for-group', 'testing', '🍕-dense-pizza-🍕']
 syrion_channels = ["That's a wipe"]
