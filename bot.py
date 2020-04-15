bdb49c9b37496876e1d4e3d46b28956693a9a7c5
Joel Couchman <joel@solidusdesigns.com.au>
2020-04-15 19:44:28 +1000

    Fix bot

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ import os
 import random
 
 from discord.ext import commands
+from jokes import jokes, sexual_jokes
 from opus_loader import load_opus_lib
 from boto.s3.connection import S3Connection
 
@@ -49,207 +50,6 @@ players = {}
 queues = {}
 volumes = {}
 voice_clients = {}
-
-sexual_jokes = [
-    "When do you kick a midget in the balls?\n\nWhen he is standing next to your girlfriend saying her hair smells nice",
-    "What's the difference between your job and a dead prostitute?\n\nYour job still sucks!",
-    "What did the hurricane say to the coconut palm tree?\n\nHold on to your nuts, this is no ordinary blow job!",
-    "How does a woman scare a gynecologist?\n\nBy becoming a ventriloquist!",
-    "Did you hear about the guy who died of a Viagra overdose?\n\nThey couldn't close his casket.",
-    "What's 6 inches long, 2 inches wide and drives women wild?\n\na $100 bill!",
-    "Whats long and hard and has cum in it?\n\na cucumber",
-    "How do you kill a circus clown?\n\nGo for the juggler!",
-    "Who was the worlds first carpenter?\n\nEve, because she made Adams banana stand",
-    "Why does Dr. Pepper come (cum) in a bottle?\n\nBecause his wife died!",
-    "If a dove is the 'bird of peace' then what's the bird of 'true love'?\n\nThe swallow.",
-    "What do you call a cheap circumcision?\n\na rip off Girl: 'Hey, what's up?' Boy: 'If I tell you, will you sit" +
-    " on it?",
-    "How do you get a nun pregnant?\n\nDress her up as an alter boy.",
-    "Why can't you play Uno with a Mexican?\n\nThey steal all the green cards.",
-    "Why don't orphans play baseball?\n\nThey don't know where home is",
-    "What's the difference between a Catholic priest and a zit?\n\nAt least a zit waits until you're a teenager " +
-    "before it cums on your face!",
-    "What does it mean when your boyfriend is in your bed gasping for breath and calling your name?\n\nYou didn't" +
-    " hold the pillow down long enough. Boy: 'Want to hear a joke about my dick? Never mind, its too long.' Girl: " +
-    "'Wanna hear a joke about my pussy? Never mind, you won't get it.",
-    "How do you tell if a chick is too fat to fuck ?\n\nWhen you pull her pants down her ass is still in them",
-    "What do you call 2 guys fighting over a slut?\n\nTug-of-whore.",
-    "If the world is a Jacket where do poor people live?\n\nIn the hood.",
-    "What's the cure for marriage?\n\nAlcoholism.",
-    "What do you call an anorexic bitch with a yeast infection?\n\nA Quarter Ponder with Cheese.",
-    "Why do they call it PMS?\n\nBecause Mad Cow Disease was already taken",
-    "How do you stop a dog from humping your leg?\n\nPick him up and suck on his cock!",
-    "Why did the chicken cross the road?\n\nBecause North Korean long-range missiles can't go that far.",
-    "What's slimy cold long and smells like pork?\n\nKermit the frogs finger",
-    "What's a porn star's favorite drink?\n\n7 Up in cider.",
-    "What's the difference between a bowling ball and a blonde?\n\nYou can only fit three fingers inside a bowling " +
-    "ball!",
-    "What do preists and McDonalds have in common?\n\nThey both stick there meat in 10 year old buns",
-    "What do you call a white guy surrounded by 9 black guys?\n\nSteve Nash.",
-    "Why can't Jesus play hockey?\n\nHe keeps getting nailed to the boards.",
-    "How do you circumcise a hillbilly?\n\nKick his sister in the jaw.",
-    "Why is the IRS going after Stormy Daniels?\n\nBecause she didn't declare all her 'gross' income.",
-    "Why do men get their great ideas in bed?\n\nBecause their plugged into a genius!",
-    "What do you call an artist with a brown finger?\n\nPiccassole",
-    "Did you guys hear about the cannibal that made a bunch of businessmen into Chili?\n\nI guess he liked seasoned" +
-    " professionals.",
-    "What's the difference between a hooker and a drug dealer?\n\nA hooker can wash her crack and sell it again.",
-    "Why was the guitar teacher arrested?\n\nFor fingering A minor.",
-    "Three words to ruin a man's ego...?\n\n'Is it in?",
-    "Whats 72?\n\n69 with three people watching",
-    "What do the Mafia and a pussy have in common?\n\nOne slip of the tongue, and you're in deep shit. A redhead " +
-    "tells her blonde stepsister, 'I slept with a Brazilian....' The blonde replies, 'Oh my God! You slut! How many " +
-    "is a brazilian?",
-    "Why don't black people go on cruises?\n\nThey already fell for that trick once.",
-    "What's the difference between a dead baby and a pizza?\n\nI don't fuck a pizza before I eat it."
-]
-
-jokes = [
-    "Crime in multi-storey car parks. That is wrong on so many different levels.",
-    "I have downloaded this new app. Its great, it tells you what to wear, what to eat and if you've put on weight. " +
-    "Its called the Daily Mail.",
-    "When I was younger I felt like a man trapped inside a woman's body. Then I was born.",
-    "I was playing chess with my friend and he said, 'Let's make this interesting'. So we stopped playing chess.",
-    "I usually meet my girlfriend at 12:59 because I like that one-to-one time.",
-    "I really wanted kids when I was in my early 20s but I could just never… lure them into my car. No, I'm kidding…" +
-    " I don't have a licence.",
-    "I was very naive sexually. My first boyfriend asked me to do missionary and I buggered off to Africa for six" +
-    " months.",
-    "One in four frogs is a leap frog.",
-    "Love is like a fart. If you have to force it it's probably shit.",
-    "I used to be addicted to swimming but I'm very proud to say I've been dry for six years.",
-    "My grandad has a chair in his shower which makes him feel old, so in order to feel young he sits on it" +
-    " backwards like a cool teacher giving an assembly about drugs.",
-    "My girlfriend is absolutely beautiful. Body like a Greek statue – completely pale, no arms.",
-    "My husband's penis is like a semi colon. I can't remember what it's for and I never use it anyway.",
-    "Is it possible to mistake schizophrenia for telepathy? I hear you ask.",
-    "I was raised as an only child, which really annoyed my sister.",
-    "I bought myself some glasses. My observational comedy improved.",
-    "You know you're working class when your TV is bigger than your book case.",
-    "Most of my life is spent avoiding conflict. I hardly ever visit Syria.",
-    "Life is like a box of chocolates. It doesn't last long if you're fat.",
-    "I was thinking of running a marathon, but I think it might be too difficult getting all the roads closed and" +
-    " providing enough water for everyone.",
-    "You can't lose a homing pigeon. If your homing pigeon doesn't come back, then what you've lost is a pigeon.",
-    "My Dad said, always leave them wanting more. Ironically, that's how he lost his job in disaster relief.",
-    "I really wish ISIS would stop playing violent video games and listening to Marilyn Manson.",
-    "There's only one thing I can't do that white people can do, and that's play pranks at international airports.",
-    "How do people make new mates? Asking for a friend.",
-    "I wanted to do a show about feminism. But my husband wouldn't let me.",
-    "One thing you'll never hear a Hindu say… 'Ah well, you only live once.",
-    "My Dad told me to invest my money in bonds. So I bought 100 copies of Goldfinger.",
-    "I've decided to stop masturbating, since then I've not really felt myself.",
-    "I always thought Trojan was a bad name for a condom brand because of course the Trojans were a people whose" +
-    " lives were ruined when a vessel containing little warriors unexpectedly exploded inside their city walls.",
-    "My wife told me: 'Sex is better on holiday.' That wasn't a nice postcard to receive.",
-    "The first time I met my wife, I knew she was a keeper. She was wearing massive gloves.",
-    "As a kid I was made to walk the plank. We couldn't afford a dog.",
-    "Money can't buy you happiness? Well, check this out, I bought myself a Happy Meal.",
-    "My father was never sexist, he beat my brothers and I equally.",
-    "The Scots invented hypnosis, chloroform and the hypodermic syringe. Wouldn't it just be easier to talk to" +
-    " a woman?",
-    "If you arrive fashionably late in Crocs, you're just late.",
-    "I can't exercise for long periods. When I get back from a run my girlfriend usually asks if I've forgotten" +
-    " something.",
-    "I saw a documentary on how ships are kept together. Riveting!",
-    "I'm learning the hokey cokey. Not all of it. But – I've got the ins and outs.",
-    "Today… I did seven press ups: not in a row.",
-    "Stephen Hawking had his first date for 10 years last week. He came back, his glasses were smashed, he had" +
-    " a broken wrist, a twisted ankle and grazed knees; apparently she stood him up!",
-    "People say I've got no willpower but I've quit smoking loads of times.",
-    "My friend got a personal trainer a year before his wedding. I thought: 'Bloody hell, how long's the aisle " +
-    "going to be'.",
-    "Golf is not just a good walk ruined, it's also the act of hitting things violently with a stick ruined.",
-    "Feminism is not a fad. It's not like Angry Birds. Although it does involve a lot of Angry Birds. Bad example.",
-    "I love languages. The way nationalities have different takes on the same thing. Like the way an Irish person" +
-    " or a Scottish person would say that the band Snow Patrol are boring but an Eskimo has a hundred words for" +
-    " how crap Snow Patrol are.",
-    "Oh my god, mega drama the other day: My dishwasher stopped working! Yup, his visa expired.",
-    "'I think jokes about learning difficulties are OK so long as they're clever' is like saying 'I think jokes" +
-    " about blind people are OK so long as they're visual'",
-    "I just bought underwater headphones and it's made me loads faster. Do you know how motivating it is swimming" +
-    " to the theme song from Jaws? I mean my anxiety is through the roof but record times.",
-    "I'm single. By choice. Her choice. No it was a mutual thing. We came to the mutual agreement that she would" +
-    " marry her ex boyfriend.",
-    "My mother told me, you don't have to put anything in your mouth you don't want to. Then she made me eat" +
-    " broccoli, which felt like double standards.",
-    "Red sky at night: shepherd's delight. Blue sky at night: day.",
-    "It all starts innocently, mixing chocolate and Rice Krispies, but before you know it you're adding raisins" +
-    " and marshmallows – it's a rocky road.",
-    "I was watching the London Marathon and saw one runner dressed as a chicken and another runner dressed as an " +
-    "egg. I thought: 'This could be interesting.",
-    "The anti-ageing advert that I would like to see is a baby covered in cream saying, 'Aah, I've used too much'",
-    "I'm sure wherever my Dad is: he's looking down on us. He's not dead, just very condescending.",
-    "Looking at my face is like reading in the car. It's all right for 10 minutes, then you start to feel sick",
-    "Doctor, doctor! Sorry mate. It's a Saturday.",
-    "Whenever I see a man with a beard, moustache and glasses, I think, 'There's a man who has taken every" +
-    " precaution to avoid people doodling on photographs of him",
-    "Miley Cyrus. You know when she was born? 1992. I've got condiments in my cupboard older than that.",
-    "'What's a couple?' I asked my mum. She said, 'Two or three'. Which probably explains why her marriage collapsed",
-    "My friend said she was giving up drinking from Monday to Friday. I'm just worried she's going to dehydrate",
-    "I have the woman-flu. Which is like the manflu but worse because I also regularly have periods and I get " +
-    "paid less.",
-    "Kim Kardashian tried to break the internet. She didn't succeed but she did leave a large visible crack.",
-    "I like Jesus but he loves me, so it's awkward.",
-    "My granny was recently beaten to death by my grandad. Not as in, with a stick – he just died first",
-    "I think if you were hardcore anti-feminism, surely you wouldn't call yourself 'anti-feminism' would you?" +
-    " You'd call yourself 'Uncle Feminism'.",
-    "My mate is called Liam, but we call him 'Two Legs Liam'. The reason for that is because he only has one arm.",
-    "I am writing a film script about going back in time to stop Hitler's parents meeting at the Austrian " +
-    "Enchantment 'Under The Sea' dance. It's called 'Back to the Fuhrer'!",
-    "My Mum was always saying that thing parents say growing up 'Wait until your dad gets home'. 'Wait until " +
-    "your dad gets home, we'll have a chat introduce you and see if he'll start paying maintenance'",
-    "'Son, I don't think you're cut out to be a mime.'\n" +
-    "'Was it something I said?' asks the son.\n" +
-    "'Yes.'",
-    "I heard a rumour that Cadbury is bringing out an oriental chocolate bar. Could be a Chinese Wispa.",
-    "I needed a password eight characters long so I picked Snow White and the Seven Dwarves.",
-    "Crash Investigations is my favourite TV show, I've seen every episode. Here's a tip for the new viewers:" +
-    " if the show starts with the pilots being interviewed… it will be a boring episode.",
-    "I think the bravest thing I've ever done is misjudge how much shopping I want to buy and still not go back" +
-    " to get a basket.",
-    "Drug use gets an unfair reputation considering all the beautiful things in life it has given us like rock" +
-    " 'n' roll and sporting achievement.",
-    "I'm not a very muscular man; the strongest thing about me is my password.",
-    "I don't have the Protestant work ethic, I have the Catholic work ethic; in that I don't work but I do feel" +
-    " very guilty about that.",
-    "I love Snapchat. I could talk about classic card games all day.",
-    "People who use selfie sticks really need to have a good, long look at themselves.",
-    "You just know Chilcot was up until 4am, downing Red Bulls and trying to crank out the last 800,000 words.",
-    "Yo Mamma's so fat… that other people have to pay for the health consequences of this via general taxation," +
-    " even though it's her responsibility.",
-    "Jokes about white sugar are rare. Jokes about brown sugar, Demerara.",
-    "A rescue cat is like recycled toilet paper. Good for the planet, but scratchy.",
-    "I bumped into my French teacher the other day who asked me what I'm up to now. I told her I go to the cinema" +
-    " and play football with my brother.",
-    "My cat is recovering from a massive stroke.",
-    "My sister had a baby and they took a while to name her and I was like, 'Hurry up!' because I didn't want my " +
-    "niece to grow up to be one of these kids you hear about on the news where it says, 'The 17 year old defendant," +
-    " who hasn't been named'.",
-    "I've always considered myself more of a lover than a fighter. Which has confused a lot of guys that have tried" +
-    " to start fights with me. They'll raise their fists, I'll whip my knob out.",
-    "I went to Waterstones and asked the woman for a book about turtles, she said 'hardback?' and I was like," +
-    " 'yeah and little heads",
-    "I learned about method acting at drama school, when all my classmates stayed in character as posh, patronising" +
-    " twats for the entire three years I was there.",
-    "My ex-girlfriend would always ask me to text her when I got in. That's how small my penis is.",
-    "I'm a comedian with irritable bowel syndrome… It's shits and giggles.",
-    "Maybe Hitler wouldn't have been so grumpy if people hadn't left him hanging for high fives all the time.",
-    "Hey, if anyone knows how to fix some broken hinges, my door's always open.",
-    "If you don't know what Morris dancing is, imagine eight guys from the KKK got lost, ended up at gay pride" +
-    " and just tried to style it out.",
-    "Hedgehogs – why can't they just share the hedge?",
-    "I think the worst thing about driving a time machine is your kids are always in the back moaning 'Are we" +
-    " then yet?'",
-    "If you don't know what introspection is, you need to take a long, hard look at yourself.",
-    "Insomnia is awful. But on the plus side – only three more sleeps till Christmas.",
-    "Centaurs shop at Topman. And Bottomhorse.",
-    "Oregon leads America in both marital infidelity and clinical depression. What a sad state of affairs.",
-    "I'm very conflicted by eye tests. I want to get the answers right but I really want to win the glasses.",
-    "Relationships are like mobile phones. You'll look at your iPhone 5 and think, it used to be a lot quicker" +
-    " to turn this thing on."
-]
-
 
 @client.event
 async def on_ready():
@@ -348,17 +148,17 @@ commands = {
         {
             'name': 'fireteam',
             'description': 'Creates a fireteam for others to join.',
-            'aliases': '!ft, !activity'
+            'aliases': ['ft', 'activity']
         },
         {
             'name': 'raid',
             'description': 'Creates a raid specific group.',
-            'aliases': '!raids'
+            'aliases': ['raids']
         },
         {
             'name': 'strike',
             'description': 'Creates a strike specific group.',
-            'aliases': '!strikes'
+            'aliases': ['strikes']
         },
         {
             'name': 'milestones',
@@ -374,12 +174,12 @@ commands = {
         {
             'name': 'play',
             'description': 'Play a song from youtube.',
-            'aliases': '!youtube, !yt'
+            'aliases': ['youtube', 'yt']
         },
         {
             'name': 'next',
             'description': 'Skips to the next song in the queue.',
-            'aliases': '!skip'
+            'aliases': ['skip']
         },
         {
             'name': 'queue',
@@ -398,7 +198,7 @@ commands = {
         {
             'name': 'joke',
             'description': 'Tells a joke.',
-            'aliases': '!jokes'
+            'aliases': ['jokes']
         },
         {
             'name': 'sexual',
@@ -424,7 +224,12 @@ async def help_command(message):
         for command in commands[group]:
             message_content += '    !{}: {}\n'.format(command['name'], command['description'])
             if 'aliases' in command.keys():
-                message_content += '        *Aliases: {}\n'.format(command['aliases'])
+                aliasString = ''
+                for aliasName in command['aliases']:
+                    if aliasString != ''
+                        aliasString += ', '
+                    aliasString += '!{}'.format(aliasName)
+                message_content += '        *Aliases: {}\n'.format(aliasString)
         message_content += '\n'
     message_content += '```'
 
@@ -596,6 +401,8 @@ def is_lfg_channel(sent_channel):
     for channel_item in lfg_channels:
         if sent_channel.name == channel_item:
             found = True
+
+    found = True # Override to allow any channel
     return found
 
 
